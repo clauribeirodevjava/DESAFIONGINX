3b83253195bf4915c9cbaa9bb45dba29ef88c23d
Lucian Tavares <lucian.sistemas.sc@gmail.com>
2023-05-08 12:46:59 -0300

    fix: Corrige pontos do desafio.

--- banco/dbx/script_db.sql
+++ banco/dbx/script_db.sql
@@ -1,4 +1,3 @@
-create database IF NOT EXISTS nodedb;
 use nodedb;
 CREATE TABLE people (
     id integer not null auto_increment,
@@ -6,8 +5,4 @@ CREATE TABLE people (
     primary key(id)
    
 );
-SET character_set_client = utf8;
-SET character_set_connection = utf8;
-SET character_set_results = utf8;
-SET collation_connection = utf8_general_ci;
-INSERT INTO user (is_active, created_date) VALUES (true,"2018-07-21");
+-- INSERT INTO user (is_active, created_date) VALUES (true,"2018-07-21");
